c00004471247eb3c9c6ad918dca21006f1dd5e11
iedfian.taufiq.akbar@gmail.com <iedfian.taufiq.akbar@gmail.com@0e1e9c6b-9ad3-4f8e-20d4-aaf9f6a0010f>
2012-07-13 06:50:45 +0000

    KajianAdapter Update

--- trunk/UstadzKita/src/com/android/ustadzkita/KajianAdapter.java
+++ trunk/UstadzKita/src/com/android/ustadzkita/KajianAdapter.java
@@ -11,25 +11,27 @@ public class KajianAdapter extends ArrayAdapter<String> {
 
 	private final Context context;
 	private final String[] Values;
-	
-	public KajianAdapter(Context context, String[] objects){
+
+	public KajianAdapter(Context context, String[] objects) {
 		super(context, R.layout.kajian_singlelist, objects);
-		
+
 		this.context = context;
 		this.Values = objects;
 	}
-	
+
 	@Override
-	public View getView(int position, View convertView, ViewGroup parent){
-		LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		View rowView = inflater.inflate(R.layout.kajian_singlelist, parent, false);
-		
+	public View getView(int position, View convertView, ViewGroup parent) {
+		LayoutInflater inflater = (LayoutInflater) context
+				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		View rowView = inflater.inflate(R.layout.kajian_singlelist, parent,
+				false);
+
 		TextView tkj_title = (TextView) rowView.findViewById(R.id.tkj_title);
-		TextView tkj_date = (TextView) rowView.findViewById(R.id.tkj_date);
-		//TextView mmm_name = (TextView) rowView.findViewById(R.id.mmm_name);
-		TextView tkj_desc = (TextView) rowView.findViewById(R.id.tkj_desc);
-		//TextView ttt_id = (TextView) rowView.findViewById(R.id.ttt_id);
-		
+		// TextView tkj_date = (TextView) rowView.findViewById(R.id.tkj_date);
+		// TextView mmm_name = (TextView) rowView.findViewById(R.id.mmm_name);
+		// TextView tkj_desc = (TextView) rowView.findViewById(R.id.tkj_desc);
+		// TextView ttt_id = (TextView) rowView.findViewById(R.id.ttt_id);
+
 		tkj_title.setText(Values[position]);
 		return rowView;
 	}
